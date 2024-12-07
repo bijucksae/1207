af9b3cebe9695e8f1ef9ba7cea0a549ae95241f9
bijucksae <jiwonmin000@gmail.com>
2024-12-07 23:02:54 +0900

    draw

--- sketch.js
+++ sketch.js
@@ -13,3 +13,28 @@ function setup() {
   mouseAttractor = new Attractor(mouseX, mouseY); 
 }
 
+function draw() {
+  background(0, 50);
+  
+  for (let attractor of attractors) {
+    attractor.display(); 
+  }
+
+  mouseAttractor.pos.set(mouseX, mouseY); 
+  mouseAttractor.display(); 
+  
+  for (let particle of particles) {
+    particle.applyForce(createVector(0, 0.05));
+    
+    for (let attractor of attractors) {
+      let force = attractor.attract(particle);
+      particle.applyForce(force);
+    }
+    
+    let mouseForce = mouseAttractor.attract(particle);
+    particle.applyForce(mouseForce);
+    
+    particle.update(); 
+    particle.display(); 
+  }
+}
